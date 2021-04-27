a8b95718df8b82e0d843c34f93f19c2ef8ea8975
Pranav Choudhary <2018184@iiitdmj.ac.in>
2021-04-28 02:31:32 +0530

    play audio on btn click

--- app/src/main/java/com/voice/voicerecorder/ui/records/RecordFragment.kt
+++ app/src/main/java/com/voice/voicerecorder/ui/records/RecordFragment.kt
@@ -1,10 +1,14 @@
 package com.voice.voicerecorder.ui.records
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.voice.voicerecorder.R
 import com.voice.voicerecorder.adapters.VoiceRecordAdapter
 
 import com.voice.voicerecorder.databinding.FragmentRecordBinding
@@ -21,6 +25,10 @@ class RecordFragment : Fragment() {
 
     private lateinit var binding: FragmentRecordBinding
     private var allFiles = ArrayList<File>()
+    private var isPlaying: Boolean = false
+    private var fileToPlay: File? = null
+    private lateinit var mediaPlayer: MediaPlayer
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,9 +43,80 @@ class RecordFragment : Fragment() {
 
         voiceRecordAdapter.setOnVoiceItemClickListener { file, position ->
 
-            //play
+            playRecording(file)
+        }
+
+
+
+    }
+
+    private fun playRecording(file: File) {
+        if (isPlaying) {
+            stopPlayingVoiceItem()
+            playVoiceItem(fileToPlay)
+        } else {
+            fileToPlay = file
+            playVoiceItem(fileToPlay)
+        }
+
+    }
+
+
+    fun stopPlayingVoiceItem() {
+        isPlaying = false
+        mediaPlayer.stop()
+        mediaPlayer.release()
+
+        binding.btmListLay.play.setImageDrawable(context?.let {
+            ActivityCompat.getDrawable(
+                it,
+                R.drawable.ic_baseline_play_arrow_24
+            )
+        })
+
+        binding.btmListLay.mediaplayerStatus.text = "Stopped"
+
+
+    }
+
+    private fun playVoiceItem(file: File?) {
+
+        isPlaying = true
+        mediaPlayer = MediaPlayer().apply {
+            setDataSource(file?.absolutePath)
+            prepare()
+        }
+
+
+        val behavior = BottomSheetBehavior.from(binding.btmListLay.root)
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+        mediaPlayer.start()
+        binding.btmListLay.filenameTv.text = file?.name
+        binding.btmListLay.mediaplayerStatus.text = "Playing"
+        binding.btmListLay.play.setImageDrawable(context?.let {
+            ActivityCompat.getDrawable(
+                it,
+                R.drawable.ic_baseline_pause_24
+            )
+        })
+
+
+        mediaPlayer.setOnCompletionListener { mp ->
+
+            stopPlayingVoiceItem()
+            binding.btmListLay.mediaplayerStatus.text = "Finished"
+
+            binding.btmListLay.play.setImageDrawable(context?.let {
+                ActivityCompat.getDrawable(
+                    it,
+                    R.drawable.ic_baseline_play_arrow_24
+                )
+            })
 
         }
+
+
 
     }
 
